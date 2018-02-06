77822b1e40401e7a45231a130774cb357e32bd1a
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-02-06 15:59:54 +0100

    Forgot Metrics.addRegistry
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -2,14 +2,29 @@ package io.vertx.starter;
 
 import com.uber.jaeger.Configuration;
 import com.uber.jaeger.metrics.MicrometerStatsReporter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+      System.setProperty("JAEGER_SERVICE_NAME", "jaeger-client-java-tester");
+      System.setProperty("JAEGER_REPORTER_LOG_SPANS", "true");
+      System.setProperty("JAEGER_SAMPLER_TYPE", "const");
+      System.setProperty("JAEGER_SAMPLER_PARAM", "1");
+      Vertx vertx = Vertx.vertx();
+      vertx.deployVerticle(new MainVerticle());
+  }
 
   @Override
   public void start() {
@@ -18,7 +33,7 @@ public class MainVerticle extends AbstractVerticle {
     Tracer tracer = configuration.getTracerBuilder().withStatsReporter(metricsReporter).build();
 
     GlobalTracer.register(tracer);
-    System.out.println("Registered tracer: " + GlobalTracer.get().toString());
+    logger.warn("Registered tracer: " + GlobalTracer.get().toString());
 
     vertx.createHttpServer()
         .requestHandler(req -> {
@@ -29,9 +44,11 @@ public class MainVerticle extends AbstractVerticle {
         .listen(8080);
 
     PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    Metrics.addRegistry(registry);
+
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end(registry.scrape()))
-        .listen(8081);
+      .requestHandler(req -> req.response().end(registry.scrape()))
+      .listen(8081);
 
   }
 
